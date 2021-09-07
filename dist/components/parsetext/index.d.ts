ce40976aa4797e7ba819f884b5494886ac90eb11
xander <shuaibualexander@gmail.com>
2021-09-07 18:45:05 +0100

    remove dist from gitignore
    merge

---
+++ dist/components/parsetext/index.d.ts
@@ -0,0 +1,4 @@
+import * as React from 'react';
+import { ParseMarkdownTextProps } from './customParser.types';
+declare const ParseMarkdownText: React.FC<ParseMarkdownTextProps>;
+export default ParseMarkdownText;
