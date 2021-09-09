1724e2b7c0dfc083cf67ea1d0ee0f2cbb8589663
xander <shuaibualexander@gmail.com>
2021-09-09 21:45:07 +0100

    update versioning

--- dist/components/parsetext/index.d.ts
+++
@@ -1,4 +0,0 @@
-import * as React from 'react';
-import { ParseMarkdownTextProps } from './customParser.types';
-declare const ParseMarkdownText: React.FC<ParseMarkdownTextProps>;
-export default ParseMarkdownText;
