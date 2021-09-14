f3da61425fbc37f777b2de34746391e777d380e3
xander <shuaibualexander@gmail.com>
2021-09-14 22:09:49 +0100

    include api based functions into sdk

---
+++ dist/index.d.ts
@@ -0,0 +1,16 @@
+/**
+ * Import all of the packages we wish to export
+ */
+import NotificationItem, { NotificationItemProps } from './components/notification/index';
+import ParseText from './components/parsetext';
+import utils from './utilities';
+import api from './utilities/api';
+import { ApiNotificationType as apiNotificationType, ParsedResponseType as parsedResponseType } from './types/notification';
+export interface ApiNotificationType extends apiNotificationType {
+}
+export interface ParsedResponseType extends parsedResponseType {
+}
+/**
+ * Export all of the public components
+ */
+export { NotificationItem, NotificationItemProps, ParseText, utils, api };
---
+++ dist/utilities/api.d.ts
@@ -0,0 +1,4 @@
+declare const _default: {
+    fetchNotifications: (userAccount: string, page?: number, itemsPerPage?: number) => Promise<any>;
+};
+export default _default;
