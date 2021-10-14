f668cb7fefcfa4b84aa4336020a3f5a86d383864
xander <shuaibualexander@gmail.com>
2021-10-14 12:13:40 +0100

    include mobile version

--- README.md
+++ README.md
@@ -22,19 +22,18 @@ In order to install and test the SDK locally, the following steps are required t
 
 - cd epns-frontend-sdk-staging // navigate to the project's directory
 
-- yarn install // to install all the packages
+- npm install // to install all the packages
 
-- yarn start //to build the project and watch out for changes
-
-- yarn link // in order to test and be made available locally
+- npm link path_to_sandbox_project/node_modules/react // in order to test and be made available locally
 // since we intend to test the functionality, we will assume that another react application is running which wants to leverage the components from the framework
 
-- npm link path_to_sandbox_project/node_modules/react
+- yarn start //to build the project and watch out for changes
   
 - cd path_to_sandbox_project
 
 - yarn link epns-frontend-sdk
 // then the library can be imported as normally would if installed using npm or yarn
+
 ```
 More information on the local testing of NPM packages can be found [here]('https://blog.logrocket.com/the-complete-guide-to-publishing-a-react-package-to-npm/")
 
--- rollup.config.js
+++ rollup.config.js
@@ -6,22 +6,46 @@ import json from '@rollup/plugin-json';
 
 import pkg from "./package.json";
 
-export default {
-  input: "src/index.tsx",
-  output: [
-    {
-      file: pkg.main,
-      format: "cjs",
-      exports: "named",
-      sourcemap: true,
-      strict: false,
-    },
-  ],
-  globals: { "styled-components": "styled" },
-  plugins: [resolve(), commonjs(), typescript(), json()],
-  external: [
-    "react", "react-dom", "styled-components",
-    "moment", "prop-types", 'html-react-parser',
-    'axios'
-  ],
-};
+export default [
+  // output for the web version
+  {
+    input: "src/web/index.tsx",
+    output: [
+      {
+        file: pkg.main,
+        format: "cjs",
+        exports: "named",
+        sourcemap: true,
+        strict: false,
+      },
+    ],
+    globals: { "styled-components": "styled" },
+    plugins: [resolve(), commonjs(), typescript(), json()],
+    external: [
+      "react", "react-dom", "styled-components",
+      "moment", "prop-types", 'html-react-parser',
+      'axios'
+    ],
+  },
+
+  // output for the mobile version
+  {
+    input: "src/mobile/index.tsx",
+    output: [
+      {
+        file: 'dist/native.js',
+        format: "cjs",
+        exports: "named",
+        sourcemap: true,
+        strict: false,
+      },
+    ],
+    globals: { "styled-components": "styled" },
+    plugins: [resolve(), commonjs(), typescript(), json()],
+    external: [
+      "react", "react-dom", "styled-components",
+      "moment", "prop-types", 'html-react-parser',
+      'axios'
+    ],
+  },
+]
