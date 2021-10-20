ab21b32a9a2d1bc15695b88854896ee210a052a9
xander <shuaibualexander@gmail.com>
2021-10-20 23:18:48 +0100

    add dummy data

--- sample_codes/loadNotifications/src/App.js
+++ sample_codes/loadNotifications/src/App.js
@@ -1,3 +1,4 @@
+import { DEFAULT_NOTIFICATIONS } from './data';
 import { NotificationItem, utils, api } from "epns-frontend-sdk-staging";
 import { useEffect, useState } from "react";
 import "./App.css";
@@ -7,32 +8,6 @@ const PAGINATION_PARAMS = {
   page: 1,
   itemsPerPage: 20,
 };
-const DEFAULT_NOTIFICATIONS = [
-  {
-    payload: {
-      data: {
-        app: "App Bot",
-        sid: "22710",
-        url: "https://epns.io/",
-        acta: "",
-        aimg: "https://backend-staging.epns.io/assets/epnsappbellturorial.jpg",
-        // aimg: "https://www.w3schools.com/html/mov_bbb.mp4",
-        // aimg: "https://www.youtube.com/watch?v=gUwXCt1qkBU",
-        amsg: "The [d:Bell] on the [b:top right] keeps track of any incoming messages and will inform you about it.\n\nClicking on the [b:bell] will update your feed [i:(Alternatively, pull feed down to refresh)]",
-        asub: "Ring the Bell",
-        icon: "https://backend-staging.epns.io/cache/bafkreibzn4s6nfa4jwyuswkojxclec5nhvj3e4ac5cvamzc2ajznh7t77a.jpg",
-        type: "1",
-        // epoch: "1625139867",
-        appbot: "1",
-        hidden: "0",
-        secret: "",
-      },
-      notification: {
-        body: "",
-      },
-    },
-  },
-]
 
 function App() {
   // create state components to fetch all the notifications.
@@ -50,7 +25,8 @@ function App() {
         const { count, results } = notificationsData || {};
         console.log(`${count} notifications loaded:`, results);
         // parse the notifications into the required format
-        const response = utils.parseApiResponse([DEFAULT_NOTIFICATIONS[0],...results]);
+        const response = utils.parseApiResponse([...DEFAULT_NOTIFICATIONS]);
+        console.log({response})
         console.log("Parsed response to:", response);
         console.log({response});
         setNotifications(response);
