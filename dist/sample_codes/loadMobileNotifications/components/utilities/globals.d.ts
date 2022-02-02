62a0ea8315fac4d7cc1eab283957f48bf029730b
xander <shuaibualexander@gmail.com>
2022-02-02 12:49:00 +0100

    deploy

---
+++ dist/sample_codes/loadMobileNotifications/components/utilities/globals.d.ts
@@ -0,0 +1,67 @@
+declare const _default: {
+    LINKS: {
+        APPBOT_NAME: string;
+        APP_WEBSITE: string;
+    };
+    STORAGE: {
+        IS_SIGNED_IN: string;
+        SIGNED_IN_TYPE: string;
+        FIRST_SIGN_IN: string;
+        USER_LOCKED: string;
+        PASSCODE_ATTEMPTS: string;
+        STORED_WALLET_OBJ: string;
+        ENCRYPTED_PKEY: string;
+        HASHED_PASSCODE: string;
+        PUSH_TOKEN: string;
+        PUSH_TOKEN_TO_REMOVE: string;
+        PUSH_TOKEN_SERVER_SYNCED: string;
+        PUSH_BADGE_COUNT: string;
+        PUSH_BADGE_COUNT_PREVIOUS: string;
+    };
+    CONSTANTS: {
+        CRED_TYPE_WALLET: string;
+        CRED_TYPE_PRIVATE_KEY: string;
+        NULL_EXCEPTION: string;
+        MAX_PASSCODE_ATTEMPTS: number;
+        PUSH_TYPE_NORMAL_MSG: number;
+        PUSH_TYPE_ENCRYPTED_MSG: number;
+        FEED_ITEMS_TO_PULL: number;
+    };
+    ADJUSTMENTS: {
+        SCREEN_GAP_HORIZONTAL: number;
+        SCREEN_GAP_VERTICAL: number;
+        DEFAULT_BIG_RADIUS: number;
+        DEFAULT_MID_RADIUS: number;
+        FEED_ITEM_RADIUS: number;
+    };
+    COLORS: {
+        PRIMARY: string;
+        LINKS: string;
+        GRADIENT_PRIMARY: string;
+        GRADIENT_SECONDARY: string;
+        GRADIENT_THIRD: string;
+        TRANSPARENT: string;
+        WHITE: string;
+        DARK_WHITE: string;
+        MID_WHITE: string;
+        LIGHT_WHITE: string;
+        SLIGHTER_GRAY: string;
+        SLIGHT_GRAY: string;
+        LIGHT_GRAY: string;
+        MID_GRAY: string;
+        DARK_GRAY: string;
+        DARKER_GRAY: string;
+        LIGHT_BLACK_TRANS: string;
+        SEMI_MID_BLACK_TRANS: string;
+        MID_BLACK_TRANS: string;
+        DARK_BLACK_TRANS: string;
+        BLACK: string;
+        CONFIRM: string;
+        WARNING: string;
+        SUBLIME_RED: string;
+        BADGE_RED: string;
+        LIGHT_MAROON: string;
+        LIGHTER_MAROON: string;
+    };
+};
+export default _default;
---
+++ dist/src/config.d.ts
@@ -0,0 +1,5 @@
+declare const _default: {
+    BASE_URL: string;
+    YOUTUBE_API_KEY: string;
+};
+export default _default;
