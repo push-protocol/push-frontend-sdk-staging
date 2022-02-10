475fb4577342a69d4ee60c9033b039034fd340c1
xander <shuaibualexander@gmail.com>
2022-02-10 23:24:14 +0100

    add final touches

---
+++ dist/src/utilities/parseMessage.d.ts
@@ -0,0 +1,7 @@
+/**
+ * @description Parse the contents of the markdown version of the notification body
+ * @param message the notification body we wish to parse
+ * @returns
+ */
+declare const FormatBody: (message: string) => string;
+export default FormatBody;
