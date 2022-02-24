50fa2caed04defa0dd4921d90b69e886d6dd057a
Aman <guptaaman200115@gmail.com>
2022-02-24 14:33:19 +0530

    Added Dark theme

--- src/web/components/notification/index.tsx
+++ src/web/components/notification/index.tsx
@@ -21,7 +21,8 @@ export type NotificationItemProps = {
   url: string | undefined;
   isSpam: boolean | undefined;
   subscribeFn: any;
-  isSubscribedFn: any
+  isSubscribedFn: any,
+  theme: string | undefined
 };
 
 type ContainerDataType = {
@@ -45,6 +46,7 @@ const ViewNotificationItem: React.FC<NotificationItemProps> = ({
   isSpam, //for rendering the spam conterpart of the notification component
   isSubscribedFn, //A function for getting if a user is subscribed to the channel in question
   subscribeFn, //A function for subscribing to the spam channel
+  theme, //for specifying light and dark theme
 }) => {
   const { notificationBody: parsedBody, timeStamp } = extractTimeStamp(
     notificationBody || ""
@@ -97,9 +99,10 @@ const ViewNotificationItem: React.FC<NotificationItemProps> = ({
       timestamp={timeStamp}
       cta={MediaHelper.validURL(cta)}
       onClick={gotToCTA}
+      theme={theme}
     >
       {/* header that only pops up on small devices */}
-      <MobileHeader onClick={goToURL}>
+      <MobileHeader onClick={goToURL} theme={theme}>
         <HeaderButton>
           <ImageContainer>
             <IPFSIcon icon={icon} />
@@ -146,10 +149,10 @@ const ViewNotificationItem: React.FC<NotificationItemProps> = ({
         {/* section for text content */}
         <ChannelDetailsWrapper>
           <ChannelTitle>
-            <ChannelTitleLink>{notificationTitle}</ChannelTitleLink>
+            <ChannelTitleLink theme={theme}>{notificationTitle}</ChannelTitleLink>
           </ChannelTitle>
           <ChannelDesc>
-            <ChannelDescLabel>
+            <ChannelDescLabel theme={theme}>
               <ParseMarkdownText text={parsedBody} />
             </ChannelDescLabel>
           </ChannelDesc>
@@ -170,7 +173,7 @@ const ViewNotificationItem: React.FC<NotificationItemProps> = ({
       <ChannelMeta hidden={!timeStamp}>
         <>
           <Pool>
-            <PoolShare>
+            <PoolShare theme = {theme}>
               {timeStamp
                 ? moment
                     .utc(parseInt(timeStamp) * 1000)
@@ -203,7 +206,8 @@ ViewNotificationItem.propTypes = {
   url: PropTypes.string,
   isSpam: PropTypes.bool,
   subscribeFn: PropTypes.func,
-  isSubscribedFn: PropTypes.func
+  isSubscribedFn: PropTypes.func,
+  theme: PropTypes.string
 };
 
 ViewNotificationItem.defaultProps = {
@@ -215,7 +219,8 @@ ViewNotificationItem.defaultProps = {
   url: "",
   isSpam: false,
   subscribeFn: null,
-  isSubscribedFn: null
+  isSubscribedFn: null,
+  theme: "light",
 };
 
 // ================= Define styled components
@@ -288,7 +293,7 @@ const Container = styled.div<ContainerDataType>`
       : "1px solid rgba(231.0, 231.0, 231.0, 1);"};
   cursor: ${(props) => (props.cta ? "pointer" : "")};
 
-  background: #fff;
+  background: ${(props) => (props.theme === "light" ? "#fff" : "#000")};
   border-radius: 10px;
 
   margin: 15px 0px;
@@ -318,7 +323,7 @@ const MobileHeader = styled.div`
     font-size: 14px;
     border-bottom: 1px solid rgba(231, 231, 231, 1);
     color: grey;
-    background: rgba(250, 250, 250, 1);
+    background: ${(props) => (props.theme === "light" ? "rgba(250, 250, 250, 1)" : "#000")};
     border-top-left-radius: 10px;
     border-top-right-radius: 10px;
     text-align: left;
@@ -344,7 +349,7 @@ const ChannelTitleLink = styled.a`
 
   @media (max-width: ${MD_BREAKPOINT}) {
     font-weight: 300;
-    color: rgba(0, 0, 0, 0.5);
+    color: ${(props) => (props.theme === "light" ? "rgba(0, 0, 0, 0.5)" : "#fff")};
   }
 `;
 
@@ -359,6 +364,7 @@ const ChannelDesc = styled.div`
 `;
 
 const ChannelDescLabel = styled.label`
+  color: ${(props) => (props.theme === "light" ? "#000" : "#fff")};
   flex: 1;
   margin: 0px;
   // font-weight: 600;
@@ -388,7 +394,7 @@ const Pool = styled.div`
 `;
 
 const PoolShare = styled(ChannelMetaBox)`
-  background: #674c9f;
+  background: ${(props) => (props.theme === "light" ? "#674c9f" : "#000")};
   @media (max-width: ${MD_BREAKPOINT}) {
     position: absolute;
     bottom: 0;
@@ -396,7 +402,7 @@ const PoolShare = styled(ChannelMetaBox)`
     border-radius: 0;
     border-radius: 8px 0;
     color: grey;
-    background: rgba(250, 250, 250, 1);
+    background: ${(props) => (props.theme === "light" ? "rgba(250, 250, 250, 1)" : "#000")};
     border-top: 1px solid rgba(231, 231, 231, 1);
     border-left: 1px solid rgba(231, 231, 231, 1);
     padding: 5px 10px;
