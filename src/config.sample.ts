60508ab9bbecd671ce38b87bd980ded53c0d769f
xander <shuaibualexander@gmail.com>
2022-04-15 10:06:28 +0100

    include and update env file

--- src/config.sample.ts
+++ src/config.sample.ts
@@ -3,6 +3,9 @@
  * and copy and fill the following parameters
  */
 export default {
-    "NOTIFICATIONS_API": "https://backend-staging.epns.io/apis/feeds/get_feeds",
-    "YOUTUBE_API_KEY": "please_input_yout_youtube_api_key"
+    "BASE_URL": "https://backend-kovan.epns.io/apis",
+    "YOUTUBE_API_KEY": "YOUTUBE_API_KEY",
+    "EPNS_CORE_CONTRACT": "0x97D7c5f14B8fe94Ef2b4bA589379f5Ec992197dA",
+    "EPNS_COMMUNICATOR_CONTRACT":"0x87da9Af1899ad477C67FeA31ce89c1d2435c77DC"
 };
+
